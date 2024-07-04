aa60a390d2d018fa60e12f87947b442ea4ebeda5
domna <florian.dobener@physik.hu-berlin.de>
2024-07-04 18:14:05 +0200

    Use central pynx mechanism for tests

--- tests/test_reader.py
+++ tests/test_reader.py
@@ -2,121 +2,32 @@
 Basic example based test for the stm reader
 """
 
-import logging
 import os
-import xml.etree.ElementTree as ET
 from pathlib import Path
 
 import pynxtools.dataconverter.convert as dataconverter
-from numpy.testing import assert_equal
-from pynxtools.dataconverter.convert import get_reader
-from pynxtools.dataconverter.helpers import generate_template_from_nxdl
-from pynxtools.dataconverter.template import Template
-from pynxtools.dataconverter.validation import validate_dict_against
-from pynxtools.definitions.dev_tools.utils.nxdl_utils import get_nexus_definitions_path
-from pynxtools.nexus.nexus import HandleNexus
-
-from pynxtools_mpes.reader import MPESReader
+from pynxtools.testing.nexus_conversion import ReaderTest
 
 
-def test_example_data(caplog):
+def test_nexus_conversion(caplog, tmp_path):
     """
-    Test the example data for the stm reader
+    Tests the conversion into nexus.
     """
-    reader = MPESReader
-    assert callable(reader.read)
-
-    def_dir = get_nexus_definitions_path()
-
-    data_dir = Path(__file__).parent / "data"
-    input_files = (
-        str(data_dir / "config_file.json"),
-        str(data_dir / "xarray_saved_small_calibration.h5"),
-    )
-
-    nxdl_file = os.path.join(def_dir, "contributed_definitions", "NXmpes.nxdl.xml")
-
-    root = ET.parse(nxdl_file).getroot()
-    template = Template()
-    generate_template_from_nxdl(root, template)
-
-    read_data = reader().read(template=Template(template), file_paths=input_files)
-
-    assert isinstance(read_data, Template)
-    with caplog.at_level(logging.WARNING):
-        assert validate_dict_against("NXmpes", read_data)
-
-    # Ensure there were no warning logs
-    assert not caplog.text
-
-
-def test_mpes_writing(tmp_path):
-    """Check if mpes example can be reproduced"""
-    # dataconverter
+    caplog.clear()
     dir_path = Path(__file__).parent / "data"
-    dataconverter.convert(
-        (
+    test = ReaderTest(
+        nxdl="NXmpes",
+        reader_name="mpes",
+        files_or_dir=[
             str(dir_path / "xarray_saved_small_calibration.h5"),
             str(dir_path / "config_file.json"),
-        ),
-        "mpes",
-        "NXmpes",
-        os.path.join(tmp_path, "mpes.small_test.nxs"),
-        False,
-        False,
+            str(dir_path / "example.nxs"),
+        ],
+        tmp_path=tmp_path,
+        caplog=caplog,
     )
-    # check generated nexus file
-    test_data = os.path.join(tmp_path, "mpes.small_test.nxs")
-    logger = logging.getLogger(__name__)
-    logger.setLevel(logging.DEBUG)
-    handler = logging.FileHandler(os.path.join(tmp_path, "mpes_test.log"), "w")
-    formatter = logging.Formatter("%(levelname)s - %(message)s")
-    handler.setLevel(logging.DEBUG)
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-    nexus_helper = HandleNexus(logger, test_data, None, None)
-    nexus_helper.process_nexus_master_file(None)
-    with open(
-        os.path.join(tmp_path, "mpes_test.log"), "r", encoding="utf-8"
-    ) as logfile:
-        log = []
-        for line in logfile.readlines():
-            if not line.startswith(
-                "DEBUG - value: https://github.com/FAIRmat-NFDI/nexus_definitions/blob/"
-            ):
-                log.append(line)
-    with open(dir_path / "Ref_nexus_mpes.log", "r", encoding="utf-8") as logfile:
-        ref_log = []
-        for line in logfile.readlines():
-            if not line.startswith(
-                "DEBUG - value: https://github.com/FAIRmat-NFDI/nexus_definitions/blob/"
-            ):
-                ref_log.append(line)
-    assert_equal(log, ref_log)
-
-
-def test_shows_correct_warnings():
-    """
-    Checks whether the read function generates the correct warnings.
-    """
-    def_dir = get_nexus_definitions_path()
-
-    data_dir = Path(__file__).parent / "data"
-    input_files = (
-        str(data_dir / "config_file.json"),
-        str(data_dir / "xarray_saved_small_calibration.h5"),
-    )
-    nxdl_file = os.path.join(def_dir, "contributed_definitions", "NXmpes.nxdl.xml")
-
-    root = ET.parse(nxdl_file).getroot()
-    template = Template()
-    generate_template_from_nxdl(root, template)
-
-    read_data = get_reader("mpes")().read(
-        template=Template(template), file_paths=tuple(input_files)
-    )
-
-    assert validate_dict_against("NXmpes", read_data)
+    test.convert_to_nexus(caplog_level="WARNING", ignore_undocumented=False)
+    test.check_reproducibility_of_nexus()
 
 
 def test_eln_data(tmp_path):
