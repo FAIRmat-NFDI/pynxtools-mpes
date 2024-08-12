c92d0c1055dd1871fe2f834fdc98933c5e3af85a
domna <florian.dobener@physik.hu-berlin.de>
2024-08-12 11:47:13 +0200

    Add tests for new eln data formalism

--- scripts/regenerate_examples.sh
+++ scripts/regenerate_examples.sh
@@ -7,7 +7,13 @@ function update_mpes_example {
   dataconverter xarray_saved_small_calibration.h5 config_file.json --reader $READER --nxdl $NXDL --output example.nxs
 }
 
+function update_mpes_eln_example {
+  echo "Update mpes example with eln file"
+  dataconverter xarray_saved_small_calibration.h5 config_file.json eln_data.yaml --reader $READER --nxdl $NXDL --output example_eln.nxs
+}
+
 project_dir=$(dirname $(dirname $(realpath $0)))
 cd $project_dir/tests/data
 
 update_mpes_example
+update_mpes_eln_example
--- tests/test_reader.py
+++ tests/test_reader.py
@@ -2,10 +2,8 @@
 Basic example based test for the stm reader
 """
 
-import os
 from pathlib import Path
 
-import pynxtools.dataconverter.convert as dataconverter
 from pynxtools.testing.nexus_conversion import ReaderTest
 
 
@@ -30,18 +28,23 @@ def test_nexus_conversion(caplog, tmp_path):
     test.check_reproducibility_of_nexus()
 
 
-def test_eln_data(tmp_path):
-    """Check if the subsections in the eln_data.yml file work."""
+def test_conversion_w_eln_data(caplog, tmp_path):
+    """
+    Tests the conversion with additional ELN data
+    """
+    caplog.clear()
     dir_path = Path(__file__).parent / "data"
-    dataconverter.convert(
-        input_file=(
+    test = ReaderTest(
+        nxdl="NXmpes",
+        reader_name="mpes",
+        files_or_dir=[
             str(dir_path / "xarray_saved_small_calibration.h5"),
             str(dir_path / "config_file.json"),
             str(dir_path / "eln_data.yaml"),
-        ),
-        reader="mpes",
-        nxdl="NXmpes",
-        output=os.path.join(tmp_path, "mpes.small_test.nxs"),
-        skip_verify=False,
-        ignore_undocumented=False,
+            str(dir_path / "example_eln.nxs"),
+        ],
+        tmp_path=tmp_path,
+        caplog=caplog,
     )
+    test.convert_to_nexus(caplog_level="WARNING", ignore_undocumented=False)
+    test.check_reproducibility_of_nexus()
