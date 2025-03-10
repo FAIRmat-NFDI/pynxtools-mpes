8bf54269411a695e3876cbe62c604d52708b860d
Laurenz Rettig <l.rettig@web.de>
2025-03-10 15:53:57 +0000

    update schema, and extend normalize function

--- src/pynxtools_mpes/nomad/lab/dataconverter.py
+++ src/pynxtools_mpes/nomad/lab/dataconverter.py
@@ -15,28 +15,59 @@ except ImportError as exc:
         "Could not import nomad package. Please install the package 'nomad-lab'."
     ) from exc
 
-from pynxtools.nomad.dataconverter import ElnYamlConverter
+from pynxtools.nomad.dataconverter import create_eln_dict, write_yaml
 
 m_package = Package(name="mpes_eln_exporter")
 
 
-class MpesElnExporter(ElnYamlConverter):
+class MpesElnExporter(EntryData):
     export = Quantity(
         type=bool,
         description="Export ELN data to YAML file",
         a_eln=dict(component="BoolEditQuantity"),
         default=True,
     )
+    output = Quantity(
+        type=str,
+        description="Output yaml file to save all the data. Default: eln_data.yaml",
+        a_eln=dict(component="StringEditQuantity"),
+        a_browser=dict(adaptor="RawFileAdaptor"),
+        default="eln_data.yaml",
+    )
 
     def normalize(self, archive, logger):
-        import debugpy
-
-        debugpy.debug_this_thread()
+        super(MpesElnExporter, self).normalize(archive, logger)
         if self.export:
             self.output = (
-                f"{archive.metadata.mainfile.replace('.archive,json','')}.yaml"
+                f"{archive.metadata.mainfile.replace('.archive.json','')}.yaml"
             )
-            super(MpesElnExporter, self).normalize(archive, logger)
+            eln_dict = create_eln_dict(archive)
+            try:
+                eln_dict["data"]["project"] = create_eln_dict(
+                    archive.data.project.m_parent
+                )["data"]
+                eln_dict["data"]["laser"] = create_eln_dict(
+                    archive.data.laser.m_parent
+                )["data"]
+                eln_dict["data"]["instrument"] = create_eln_dict(
+                    archive.data.instrument.m_parent
+                )["data"]
+                eln_dict["data"]["sample"] = create_eln_dict(
+                    archive.data.sample.m_parent
+                )["data"]
+                for index, user in enumerate(archive.data.project.users):
+                    eln_dict["data"][f"user{index}"] = create_eln_dict(user.m_parent)[
+                        "data"
+                    ]
+                del eln_dict["data"]["export"]
+                del eln_dict["data"]["project"]["instrument_status"]
+                del eln_dict["data"]["project"]["laser_status"]
+                del eln_dict["data"]["project"]["users"]
+                del eln_dict["data"]["project"]["samples"]
+            except AttributeError as e:
+                print(e)
+                pass
+            write_yaml(archive, archive.data.output, eln_dict)
 
 
 m_package.__init_metainfo__()
