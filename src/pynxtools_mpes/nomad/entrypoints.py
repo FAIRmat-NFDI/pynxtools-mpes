689d0c099e00281e9ba0c7930ad60805a286f419
Laurenz Rettig <l.rettig@web.de>
2025-03-12 10:53:32 +0000

    fix merge issue

--- src/pynxtools_mpes/nomad/entrypoints.py
+++ src/pynxtools_mpes/nomad/entrypoints.py
@@ -308,15 +308,6 @@ mpes_app = AppEntryPoint(
                         "lg": {"minH": 3, "minW": 3, "h": 6, "w": 6, "y": 0, "x": 12}
                     },
                 },
-                {
-                    "type": "scatter_plot",
-                    "x": "data.ENTRY[*].data.data__size#pynxtools.nomad.schema.Root#int",
-                    "y": "data.ENTRY[*].collection_time__field#pynxtools.nomad.schema.Root#int",
-                    "title": "ScanQuality",
-                    "layout": {
-                        "lg": {"minH": 3, "minW": 3, "h": 6, "w": 6, "y": 10, "x": 18}
-                    },
-                },
             ]
         },
     ),
